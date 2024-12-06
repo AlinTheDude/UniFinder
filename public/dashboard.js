aca108220780d3aac89e2d42ce324922c2930b13
AlinTheDude <129039059+AlinTheDude@users.noreply.github.com>
2024-12-06 12:46:08 +0000

    TST

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Recupera i dati dell'utente da sessionStorage
     const email = sessionStorage.getItem('userEmail');
     if (!email) {
-        window.location.href = 'login.html'; // Reindirizza se non c'è l'email
+        window.location.href = 'login.html'; // Se non è presente, reindirizza alla login
     }
     
     document.getElementById('email').innerText = email;
     
+    // Chiamata API per ottenere il nome dell'utente
     fetch(`http://65.108.146.104:3001/utente?email=${email}`)
         .then(response => response.json())
         .then(data => {
             document.getElementById('username').innerText = data.nome || 'Non specificato';
         });
 
-    // Menu hamburger per mobile
+    // Gestione del menu mobile
     const hamburger = document.querySelector('.hamburger-menu');
     const navbarMenu = document.querySelector('.navbar-menu');
 
