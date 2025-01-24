31d285c6b0f897c8d1686346ec9a7903c6bb121f
AlinTheDude <129039059+AlinTheDude@users.noreply.github.com>
2025-01-24 08:42:29 +0000

    test

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   google: {
-      clientId: '630061902452-lrubn0joaj9pt5hhrq2e2k7nvfqsgep4.apps.googleusercontent.com',
-      callbackURL: 'http://65.108.146.104:3001/auth/google/callback'
+
   }
 };
